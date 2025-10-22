5e29eeee8095b584193802173619493ebda393fd
Xiangzhuang Shen <datahonor@gmail.com>
2025-10-22 20:56:23 +0800

    fix: dataset loader

--- toynlp/gpt/dataset.py
+++ toynlp/gpt/dataset.py
@@ -1,31 +1,31 @@
-from datasets import Dataset, load_dataset
+from datasets import load_dataset
 from tokenizers import Tokenizer
 import torch
 from torch.utils.data import DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 from toynlp.gpt.config import GPTConfig
 from toynlp.gpt.tokenizer import GPTTokenizer
 
 
-def get_dataset(
-    dataset_path: str,
-    dataset_name: str | None,
-    split: str,
-) -> Dataset:
-    dataset = load_dataset(path=dataset_path, name=dataset_name, split=split, streaming=True)
-    return dataset  # type: ignore[return-value]
-
-
-def split_text_into_contexts(texts: str, max_length: int, tokenizer: Tokenizer) -> list[torch.Tensor]:
+def split_text_into_contexts(texts: list[str], max_length: int, tokenizer: Tokenizer) -> torch.Tensor:
     contexts = []
+    # print(f"len texts: {len(texts)}")
     for text in texts:
-        token_ids = tokenizer.encode(text).ids  # type: ignore[call-arg,index]
+        # print(f"Processing text of length {len(text)}")
+        token_ids = tokenizer.encode(text).ids
         for i in range(len(token_ids) // max_length + 1):
             start_idx = i * max_length
             end_idx = (i + 1) * max_length
-            if end_idx < len(token_ids):
-                contexts.append(torch.tensor(token_ids[start_idx:end_idx], dtype=torch.long))
-    return contexts
+            # print(f"i: {i}, start_idx: {start_idx}, end_idx: {end_idx}, len(token_ids): {len(token_ids)}")
+            if start_idx < len(token_ids):
+                contexts.append(token_ids[start_idx:end_idx])
+    context_tensor = pad_sequence(
+        [torch.tensor(context, dtype=torch.long) for context in contexts],
+        batch_first=True,
+        padding_value=tokenizer.token_to_id("<pad>"),
+    )
+    return context_tensor
 
 
 def get_split_dataloader(
@@ -34,7 +34,7 @@ def get_split_dataloader(
     config: GPTConfig,
     gpt_tokenizer: Tokenizer,
 ) -> DataLoader:
-    raw_dataset = load_dataset(path=dataset_path, name=None, split=split)
+    raw_dataset = load_dataset(path=dataset_path, split=split)
     num_shards = min(32, raw_dataset.num_rows)  # type: ignore[possible-missing-attribute]
     raw_dataset = raw_dataset.shuffle(seed=42).to_iterable_dataset(num_shards=num_shards)  # type: ignore[call-arg]
     context_dataset = raw_dataset.map(
@@ -50,11 +50,11 @@ def get_split_dataloader(
     )
 
     dataloader = torch.utils.data.DataLoader(
-        dataset=context_dataset,
+        dataset=context_dataset.with_format("torch"),
         batch_size=config.batch_size,
         num_workers=8,
         prefetch_factor=4,
-        drop_last=True,
+        drop_last=False,
     )
 
     return dataloader
@@ -69,14 +69,15 @@ if __name__ == "__main__":
 
     demo_dataset_loader = get_split_dataloader(
         config.dataset_path,
-        "train[:1%]",
+        "train[:1]",
         config,
         gpt_tokenizer,
     )
     # print(f"Number of training batches: {len(val_dataset_loader)}")
-    for i, batch in enumerate(tqdm(demo_dataset_loader)):
+    for _, batch in enumerate(tqdm(demo_dataset_loader)):
         # Process each batch
-        # print(batch)
         print(batch["input_ids"].shape, batch["input_ids"].dtype)
-        print(batch["input_ids"][0][:10])
-        print("=" * 20, i, "=" * 20)
+
+    # for j in range(len(batch["input_ids"])):
+    #     print(gpt_tokenizer.decode(batch["input_ids"][j].tolist(), skip_special_tokens=False))
+    #     print("-" * 20, j, "-" * 20)
